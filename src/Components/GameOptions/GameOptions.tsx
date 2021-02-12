d505c235bfdf0e2934b77573c015fec46df49759
sergeyLokhin <lokhin@uchi.ru>
2021-02-12 22:28:23 +0300

    GameOptions styles

--- src/Components/GameOptions/GameOptions.tsx
+++ src/Components/GameOptions/GameOptions.tsx
@@ -5,10 +5,37 @@ import styled from "@emotion/styled";
 
 const OptionsWrapper = styled.div`
   display: flex;
-  margin: auto;
+  flex-direction: column;
+  margin: 20px auto 0px;
   justify-content: space-around;
   align-items: center;
-  width: 400px;
+`;
+
+const OptionsRow = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+  &:last-of-type {
+    margin-bottom: 0px;
+  }
+`;
+
+const LabelWrapper = styled.div`
+  width: 170px;
+  margin-right: 15px;
+  font-size: 24px;
+  text-align: right;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+  font-weight: bold;
+  color: #2f42d0;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 300px;
 `;
 
 const BlueButton = withStyles(() => ({
@@ -91,30 +118,64 @@ export class GameOptions extends React.Component<
   render(): React.ReactNode {
     return (
       <OptionsWrapper className={"options-wrapper"}>
-        <BlueButton
-          classes={{ root: "options-button-size-small" }}
-          variant={"contained"}
-          color={"primary"}
-          onClick={() => this.setFieldSize("small")}
-        >
-          Small
-        </BlueButton>
-        <LightBlueButton
-          classes={{ root: "options-button-size-medium" }}
-          variant={"contained"}
-          color={"primary"}
-          onClick={() => this.setFieldSize("medium")}
-        >
-          Medium
-        </LightBlueButton>
-        <LightGreenButton
-          classes={{ root: "options-button-size-large" }}
-          variant={"contained"}
-          color={"primary"}
-          onClick={() => this.setFieldSize("large")}
-        >
-          Large
-        </LightGreenButton>
+        <OptionsRow className={"options-row"}>
+          <LabelWrapper className={"options-label"}>Field size:</LabelWrapper>
+          <ButtonWrapper className={"options-button-wrapper"}>
+            <BlueButton
+              classes={{ root: "options-button-size-small" }}
+              variant={"contained"}
+              color={"primary"}
+              onClick={() => this.setFieldSize("small")}
+            >
+              Small
+            </BlueButton>
+            <LightBlueButton
+              classes={{ root: "options-button-size-medium" }}
+              variant={"contained"}
+              color={"primary"}
+              onClick={() => this.setFieldSize("medium")}
+            >
+              Medium
+            </LightBlueButton>
+            <LightGreenButton
+              classes={{ root: "options-button-size-large" }}
+              variant={"contained"}
+              color={"primary"}
+              onClick={() => this.setFieldSize("large")}
+            >
+              Large
+            </LightGreenButton>
+          </ButtonWrapper>
+        </OptionsRow>
+        <OptionsRow className={"options-row"}>
+          <LabelWrapper className={"options-label"}>Game speed:</LabelWrapper>
+          <ButtonWrapper className={"options-button-wrapper"}>
+            <BlueButton
+              classes={{ root: "options-button-speed-slow" }}
+              variant={"contained"}
+              color={"primary"}
+              onClick={() => this.setSpeed("slow")}
+            >
+              Slow
+            </BlueButton>
+            <LightBlueButton
+              classes={{ root: "options-button-speed-medium" }}
+              variant={"contained"}
+              color={"primary"}
+              onClick={() => this.setSpeed("medium")}
+            >
+              Medium
+            </LightBlueButton>
+            <LightGreenButton
+              classes={{ root: "options-button-speed-fast" }}
+              variant={"contained"}
+              color={"primary"}
+              onClick={() => this.setSpeed("fast")}
+            >
+              Fast
+            </LightGreenButton>
+          </ButtonWrapper>
+        </OptionsRow>
       </OptionsWrapper>
     );
   }

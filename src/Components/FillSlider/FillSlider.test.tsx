a3af2830ad25f04247563d5dcd7a6020d5363ea7
sergeyLokhin <lokhin@uchi.ru>
2021-02-15 16:57:59 +0300

    fix FillSlider test

--- src/Components/FillSlider/FillSlider.test.tsx
+++ src/Components/FillSlider/FillSlider.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { mount } from "enzyme";
 import { FillSlider } from "./FillSlider";
+import { Slider } from "@material-ui/core";
 
 describe("FillSlider test", () => {
   it("drag FillSlider", () => {
@@ -13,19 +14,16 @@ describe("FillSlider test", () => {
       fillSlider.find("input[name='fill-percent-input']").at(0).prop("value")
     ).toEqual("30");
 
-    fillSlider
-      .find("input[name='fill-percent-input']")
-      .at(0)
-      .simulate("change", { target: { value: 55 } });
+    expect(fillSlider.find(Slider).length).toBe(1);
+
+    const slider = fillSlider.find(Slider).at(0);
+
+    slider.props().onChange!({} as React.ChangeEvent, 75);
 
     expect(setFilledPercent).toHaveBeenCalled();
     expect(setFilledPercent).toHaveBeenCalledTimes(1);
     expect(setFilledPercent.mock.calls[0][0]).toMatchObject({
-      percent: 55,
+      percent: 75,
     });
-
-    expect(
-      fillSlider.find("input[name='fill-percent-input']").at(0).prop("value")
-    ).toEqual("55");
   });
 });

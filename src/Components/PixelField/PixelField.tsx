98e7e2ab00b3eb7f05127ec95764b375ff64fdb8
sergeyLokhin <lokhin@uchi.ru>
2021-02-26 22:48:47 +0300

    PixelField functional component

--- src/Components/PixelField/PixelField.tsx
+++ src/Components/PixelField/PixelField.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { FC } from "react";
 import { Pixel } from "../Pixel/Pixel";
 
 interface PixelFieldProps {
@@ -6,44 +6,39 @@ interface PixelFieldProps {
   onPixelClick: (coordX: number, coordY: number, newFlag: boolean) => void;
 }
 
-export class PixelField extends Component<
-  PixelFieldProps,
-  Record<string, unknown>
-> {
-  render(): React.ReactNode {
-    const { onPixelClick, pixelStatesMatrix } = this.props;
-    return (
-      <div
-        className="pixel-field"
-        style={{ display: "inline-block", border: "2px solid #1a1a1a" }}
-      >
-        {pixelStatesMatrix.reduce(
-          (
-            result: Array<React.ReactNode[] | React.ReactNode>,
-            row: boolean[],
-            x: number
-          ): Array<React.ReactNode[] | React.ReactNode> => {
-            result.push(
-              row.map(
-                (filled: boolean, y: number): React.ReactNode => {
-                  return (
-                    <Pixel
-                      key={`${x}-${y}`}
-                      filled={filled}
-                      x={x}
-                      y={y}
-                      onClick={onPixelClick}
-                    />
-                  );
-                }
-              ),
-              <br key={x} />
-            );
-            return result;
-          },
-          [] as Array<React.ReactNode[] | React.ReactNode>
-        )}
-      </div>
-    );
-  }
-}
+export const PixelField: FC<PixelFieldProps> = (props: PixelFieldProps) => {
+  const { pixelStatesMatrix, onPixelClick } = props;
+  return (
+    <div
+      className="pixel-field"
+      style={{ display: "inline-block", border: "2px solid #1a1a1a" }}
+    >
+      {pixelStatesMatrix.reduce(
+        (
+          result: Array<React.ReactNode[] | React.ReactNode>,
+          row: boolean[],
+          x: number
+        ): Array<React.ReactNode[] | React.ReactNode> => {
+          result.push(
+            row.map(
+              (filled: boolean, y: number): React.ReactNode => {
+                return (
+                  <Pixel
+                    key={`${x}-${y}`}
+                    filled={filled}
+                    x={x}
+                    y={y}
+                    onClick={onPixelClick}
+                  />
+                );
+              }
+            ),
+            <br key={x} />
+          );
+          return result;
+        },
+        [] as Array<React.ReactNode[] | React.ReactNode>
+      )}
+    </div>
+  );
+};

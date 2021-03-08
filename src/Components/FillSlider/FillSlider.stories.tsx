4c45b55be28000d43ca279abc8d36dcd900bd2cc
sergeyLokhin <lokhin@uchi.ru>
2021-03-08 22:51:41 +0300

    FillSlider story, using useState hook

--- src/Components/FillSlider/FillSlider.stories.tsx
+++ src/Components/FillSlider/FillSlider.stories.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { FillSlider } from "./FillSlider";
 import { DEFAULT_SLIDER_PERCENT } from "../App/App";
@@ -7,40 +7,13 @@ export default {
   decorators: [withKnobs],
 };
 
-interface SliderWrapperStoryState {
-  storyPercent: number;
-}
-
-class SliderWrapperStory extends Component<
-  Record<string, unknown>,
-  SliderWrapperStoryState
-> {
-  state = {
-    storyPercent: DEFAULT_SLIDER_PERCENT,
-  };
-
-  setStoryPercent = (percent: number): void => {
-    this.setState({
-      storyPercent: percent,
-    });
-  };
-
-  setFilledPercent = (percent: number): void => {
-    this.setStoryPercent(percent);
-  };
-
-  render(): React.ReactNode {
-    const { storyPercent } = this.state;
-    return (
-      <FillSlider
-        defaultPercent={DEFAULT_SLIDER_PERCENT}
-        currentPercent={storyPercent}
-        setFilledPercent={this.setFilledPercent}
-      ></FillSlider>
-    );
-  }
-}
-
-export const fillSliderDefault = (): React.ReactNode => {
-  return <SliderWrapperStory></SliderWrapperStory>;
+export const SliderStory = (): React.ReactNode => {
+  const [value, setValue] = useState(DEFAULT_SLIDER_PERCENT);
+  return (
+    <FillSlider
+      defaultPercent={DEFAULT_SLIDER_PERCENT}
+      currentPercent={value}
+      setFilledPercent={setValue}
+    />
+  );
 };
